4a71497778ee6c5536ec4a4024f9f6dff96fdff2
Jean Vallee <jean.vallee@gmx.fr>
2026-05-19 02:30:40 +0200

    git ignore airflow servers' files 3rd try

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Project shared library (multi-phase).
-
-This package contains reusable code shared across phases.
-Phase 1 focuses on ETL (MongoDB -> PostgreSQL), but later phases can add more
-modules while keeping imports and configuration consistent.
-"""
-
